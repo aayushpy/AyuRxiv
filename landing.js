ee732acf76d79903d23cd504b9c277ff415f91a9
Aayush Pandey <65813937+aayushpy@users.noreply.github.com>
2024-04-15 16:02:28 +0800

    Update landing.js

--- landing.js
+++ landing.js
@@ -1,16 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export default {
-  async fetch(request, env, ctx) {
-    const htmlContent = `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -41,13 +28,3 @@ export default {
         </div>
     </body>
     </html>
-    
-    `;
-
-    return new Response(htmlContent, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8",
-      },
-    });
-  },
-};
